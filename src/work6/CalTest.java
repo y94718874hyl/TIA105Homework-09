4233881c593223f247f0301ac6c3927a7be209fc
YL.H <y94718874hyl@gmail.com>
2025-01-17 13:51:19 +0800

    HW6 Cal better N4

--- src/work6/CalTest.java
+++ src/work6/CalTest.java
@@ -7,32 +7,15 @@ public class CalTest {
 	public static void main(String[] args) {
 		int x = 0, y = 0, re;
 		Scanner sc = new Scanner(System.in);
-
 		Calculator c = new Calculator();
 
 		while (true)
 			try {
-				
-				//排除x非數字
 				System.out.println("請輸入x的值");
-				if(!sc.hasNextInt()) {
-					System.out.println("輸入格式不正確");
-					sc.nextLine();
-					continue;
-				}else if (sc.hasNextInt()) {
-					x = sc.nextInt();
-				}
-				
-				//排除y非數字
+				x = c.checkInt(sc, "x");
 				System.out.println("請輸入y的值");
-				if(!sc.hasNextInt()) {
-					System.out.println("輸入格式不正確");
-					sc.nextLine();
-					continue;
-				}else if (sc.hasNextInt()) {
-					y = sc.nextInt();
-				}
-				
+				y = c.checkInt(sc, "y");
+
 				re = c.powerXY(x, y);
 				if (x > 0 && y > 0) {
 					System.out.println(x + "的" + y + "次方" + "等於" + re);
@@ -42,6 +25,5 @@ public class CalTest {
 			} catch (CalException e) {
 				System.out.println(e.getMessage());
 			}
-
 	}
 }
--- src/work6/Calculator.java
+++ src/work6/Calculator.java
@@ -1,16 +1,13 @@
 package work6;
 
-
 import java.util.Scanner;
 
 public class Calculator {
 	private int x;
 	private int y;
 	int re;
-	
 
 	public Calculator() {
-
 	}
 
 	public int powerXY(int x, int y) throws CalException {
@@ -24,15 +21,41 @@ public class Calculator {
 		} else if (x > 0 && y > 0) {
 			int re = (int) Math.pow(x, y);
 			return re;
-			
 
 		} else {
 			throw new CalException("輸入格式不正確");
-		
-			
 		}
-		
-
 	}
 
+	public int checkInt(Scanner sc, String data) {
+		while (!sc.hasNextInt()) {
+			System.out.println("輸入格式不正確");
+			System.out.println("請輸入" + data + "的值");
+			sc.next();
+			continue;
+		}
+		if (sc.hasNextInt()) {
+
+		}
+		return sc.nextInt();
+	}
 }
+////排除x非數字
+//System.out.println("請輸入x的值");
+//if(!sc.hasNextInt()) {
+//	System.out.println("輸入格式不正確");
+//	sc.nextLine();
+//	continue;
+//}else if (sc.hasNextInt()) {
+//	x = sc.nextInt();
+//}
+//
+////排除y非數字
+//System.out.println("請輸入y的值");
+//if(!sc.hasNextInt()) {
+//	System.out.println("輸入格式不正確");
+//	sc.nextLine();
+//	continue;
+//}else if (sc.hasNextInt()) {
+//	y = sc.nextInt();
+//}
